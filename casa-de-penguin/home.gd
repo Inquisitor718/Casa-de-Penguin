9d92d90fdc55a078abd57a75ecc3d5b6e31992df
Inquisitor718 <shreyash.anand718@gmail.com>
2026-01-24 04:37:37 +0530

    parsing error fixed

--- casa-de-penguin/home.gd
+++ casa-de-penguin/home.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
-@export var Snowman: PackedScene
-@export var Pond: PackedScene
-@export var Fireplace: PackedScene  
-@export var Kitchen: PackedScene  
+
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,19 +12,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_packed(Snowman)
-
-
-func _on_pond_button_pressed() -> void:
-	get_tree().change_scene_to_packed(Pond)
-
-
-func _on_fireplace_pressed() -> void:
-	get_tree().change_scene_to_packed(Fireplace)
-
-
-func _on_kitchen_pressed() -> void:
-	get_tree().change_scene_to_packed(Kitchen)
