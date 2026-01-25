52a08c2adb45992d2d7bc8c52a033f7a9cfa30fe
Ansh Bansal <anshbansal7433@gmail.com>
2026-01-25 15:58:54 +0530

    Cam

--- casa-de-penguin/home.gd
+++ casa-de-penguin/home.gd
@@ -1,4 +1,6 @@
 extends Node2D
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var snowman_button: TextureButton = $"Snowman Button"
 
 
 
@@ -7,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ProgresBar.Decay_Rate = 0.5
-
+	camera_2d.global_position=Vector2(963,542)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,7 +17,24 @@ func _process(delta: float) -> void:
 
 
 func _on_snowman_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Snowman.tscn")
+	var tween_snowman_move := create_tween()
+	tween_snowman_move.tween_property(
+		camera_2d,
+		"global_position",
+		Vector2(1633,241),
+		0.5
+		
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	
+	var tween_snowman_zoom := create_tween()
+	tween_snowman_zoom.tween_property(
+		camera_2d,
+		"zoom",
+		Vector2(1922/snowman_button.rect_size.x,1078/snowman_button.rect_size.y),
+		0.5
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	
+	
 
 
 func _on_pond_button_pressed() -> void:
