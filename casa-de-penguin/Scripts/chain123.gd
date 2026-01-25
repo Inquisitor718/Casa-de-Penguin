443cbab44c32e2e82e709df0d61471a048737837
Vineet-19 <diwatevineet19@gmail.com>
2026-01-25 16:42:53 +0530

    chain fixed

--- casa-de-penguin/Scripts/chain123.gd
+++ casa-de-penguin/Scripts/chain123.gd
@@ -18,6 +18,17 @@ func _process(delta: float) -> void:
 	if !drag:
 		chain_123.position.y = (max(0,chain_123.position.y-velocity * delta))
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
+		drag = false
+		
+	if drag and event is InputEventMouseMotion:
+		var vel = event.relative.y
+		chain_123.position.y = min((max(0,chain_123.position.y+vel)),750)
+
+		if vel > spill_thershold and not spill:
+			spill = true
+			print("spill")
 
 func _on_chain_123_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -27,15 +38,3 @@ func _on_chain_123_input_event(viewport: Node, event: InputEvent, shape_idx: int
 		else:
 			drag = false
 		return
-
-	if drag and event is InputEventMouseMotion:
-		var vel = event.relative.y
-		chain_123.position.y = min((max(0,chain_123.position.y+vel)),750)
-
-		if vel > spill_thershold and not spill:
-			spill = true
-			print("spill")
-
-
-func _on_chain_123_mouse_exited() -> void:
-	drag = false
